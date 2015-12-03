b04495d0d5668fbcedf2e79c0da45423b255ecba
Ellery Queen <429889664@qq.com>
2015-12-03 16:23:35 +0800

    具体数据的文件输入输出
    输出完成，输入未完成；因实验需求理解错误，这两个类的开发流产

---
+++ src/com/kururu/fileIO.java
@@ -0,0 +1,145 @@
+package com.kururu;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by kururu on 2015/12/1.
+ */
+public class fileIO {
+    public static void input2File(String mes,File file){
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter(file);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        BufferedWriter bw = new BufferedWriter(fw);
+
+        try {
+            bw.write(mes);
+            bw.flush();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }finally{
+            try {
+                if(bw!=null){
+
+                    bw.close();
+                }
+
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public static void input2File(@SuppressWarnings("rawtypes") List mes,File file){
+
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter(file);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        BufferedWriter bw = new BufferedWriter(fw);
+
+
+        try {
+            for(Object o : mes){
+                bw.write(o.toString());
+                bw.newLine();
+            }
+            bw.flush();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }finally{
+            try {
+                if(bw!=null){
+                    bw.close();
+                }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        }
+    }
+    //读取一行记录用此方法
+    public static String outputFromFile(String filePath){
+        FileReader fr = null;
+        try {
+            fr = new FileReader(filePath);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        BufferedReader br = new BufferedReader(fr);
+        String s = null;
+        try {
+            s = br.readLine();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }finally{
+            if(br!=null){
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return s;
+    }
+    //读出文件内的所有记录
+    public static List<String> outputListFromFile(String filePath){
+
+        List<String> list = new ArrayList<String>();
+        FileReader fr = null;
+        try {
+            fr = new FileReader(filePath);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        BufferedReader br = new BufferedReader(fr);
+        String s = null;
+
+        try {
+            while((s = br.readLine())!=null){
+                list.add(s);
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }finally{
+            if(br!=null){
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return list;
+    }
+
+}
